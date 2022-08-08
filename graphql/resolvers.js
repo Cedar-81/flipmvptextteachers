916b2c238fd818c2a57b42f8a893ddb8b8fffad8
Cedar-81 <64336732+Cedar-81@users.noreply.github.com>
2022-08-07 18:55:22 -1100

    gmail bug fix

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -6,66 +6,71 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (val) => {
   let testAccount = await nodemailer.createTestAccount();
-  var smtpTransport = require("nodemailer-smtp-transport");
-  console.log(
-    process.env.NEXT_PUBLIC_APP_EMAIL,
-    process.env.NEXT_PUBLIC_APP_PASSWORD
-  );
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport(
-    smtpTransport({
-      service: "gmail",
-      host: "smtp.gmail.com",
-      auth: {
-        user: `${process.env.NEXT_PUBLIC_APP_EMAIL}`, // generated ethereal user
-        pass: `${process.env.NEXT_PUBLIC_APP_PASSWORD}`, // generated ethereal password
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    })
-  );
+  try {
+    var smtpTransport = require("nodemailer-smtp-transport");
+    console.log(
+      process.env.NEXT_PUBLIC_APP_EMAIL,
+      process.env.NEXT_PUBLIC_APP_PASSWORD
+    );
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport(
+      smtpTransport({
+        service: "gmail",
+        host: "smtp.gmail.com",
+        auth: {
+          user: `${process.env.NEXT_PUBLIC_APP_EMAIL}`, // generated ethereal user
+          pass: `${process.env.NEXT_PUBLIC_APP_PASSWORD}`, // generated ethereal password
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      })
+    );
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: `"Flip Classroom" <readatetech@gmail.com>`, // sender address
-    to: `${val.to_email}`, // list of receivers
-    subject: "Flip Classroom Email Verification", // Subject line
-    // text: "Hello world?", // plain text body
-    html: `<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,600;1,700;1,800;1,900&family=Inter:wght@400;500;600;700;800;900&family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto:wght@300&display=swap"
-      rel="stylesheet"
-    />
-    <title>Flip Classroom Email Verification</title>
-  </head>
-  <body style="font-family: 'Fira Sans', sans-serif">
-    <div
-      style="
-        height: max-content;
-        width: 100%;
-        background-color: rgb(27, 27, 27);
-        text-align: center;
-        color: white;
-        padding: 2rem 0;
-      "
-    >
-      <h2 style="">Email Verification</h2>
-      <h3 style="width: 90%; margin: auto; font-size: small">
-        This is an email verification code from flip classroom, if you didn't
-        signup for this please ignore
-      </h3>
-      <p style="font-size: 50px">Code: ${val.message}</p>
-    </div>
-  </body>
-</html>`, // html body
-  });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: `"Flip Classroom" <readatetech@gmail.com>`, // sender address
+      to: `${val.to_email}`, // list of receivers
+      subject: "Flip Classroom Email Verification", // Subject line
+      // text: "Hello world?", // plain text body
+      html: `<html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,600;1,700;1,800;1,900&family=Inter:wght@400;500;600;700;800;900&family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto:wght@300&display=swap"
+        rel="stylesheet"
+      />
+      <title>Flip Classroom Email Verification</title>
+    </head>
+    <body style="font-family: 'Fira Sans', sans-serif">
+      <div
+        style="
+          height: max-content;
+          width: 100%;
+          background-color: rgb(27, 27, 27);
+          text-align: center;
+          color: white;
+          padding: 2rem 0;
+        "
+      >
+        <h2 style="">Email Verification</h2>
+        <h3 style="width: 90%; margin: auto; font-size: small">
+          This is an email verification code from flip classroom, if you didn't
+          signup for this please ignore
+        </h3>
+        <p style="font-size: 50px">Code: ${val.message}</p>
+      </div>
+    </body>
+  </html>`, // html body
+    });
+    console.log(info);
+  } catch (e) {
+    return e;
+  }
 };
 
 export const resolvers = {
