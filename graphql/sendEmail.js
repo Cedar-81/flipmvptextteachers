a9ab4400388d2a3af10ff990c33cf0f25c3025a5
Cedar-81 <64336732+Cedar-81@users.noreply.github.com>
2022-08-07 22:51:23 -1100

    implemented oauth2 with nodemailer and gmail for more secure email transfer

---
+++ graphql/sendEmail.js
@@ -0,0 +1,91 @@
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+const nodemailer = require("nodemailer");
+
+const oauth2Client = new OAuth2(
+  `${process.env.NEXT_PUBLIC_CLIENT_ID}`, // ClientID
+  `${process.env.NEXT_PUBLIC_CLIENT_SECRET}`, // Client Secret
+  "https://developers.google.com/oauthplayground" // Redirect URL
+);
+
+oauth2Client.setCredentials({
+  refresh_token: `${process.env.NEXT_PUBLIC_REFRESH_TOKEN}`,
+});
+
+const accessToken = oauth2Client.getAccessToken();
+
+export default async function sendEmail(val) {
+  //   await sendEmail1(val);
+  await sendEmail1(val);
+}
+
+const sendEmail1 = async (val) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: `${process.env.NEXT_PUBLIC_APP_EMAIL}`,
+        clientId: `${process.env.NEXT_PUBLIC_CLIENT_ID}`,
+        clientSecret: `${process.env.NEXT_PUBLIC_CLIENT_SECRET}`,
+        refreshToken: `${process.env.NEXT_PUBLIC_REFRESH_TOKEN}`,
+        accessToken: accessToken,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: `"Flip Classroom" <${process.env.NEXT_PUBLIC_APP_EMAIL}>`, // sender address
+      to: `${val.to_email}`, // list of receivers
+      subject: "Flip Classroom Email Verification", // Subject line
+      // text: "Hello world?", // plain text body
+      html: genHtmlText(val), // html body
+    });
+    console.log(info);
+  } catch (e) {
+    console.log(e);
+    transporter.close();
+    return e;
+  }
+};
+
+const genHtmlText = (val) => {
+  return `
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,600;1,700;1,800;1,900&family=Inter:wght@400;500;600;700;800;900&family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto:wght@300&display=swap"
+        rel="stylesheet"
+      />
+      <title>Flip Classroom Email Verification</title>
+    </head>
+    <body style="font-family: 'Fira Sans', sans-serif">
+      <div
+        style="
+          height: max-content;
+          width: 100%;
+          background-color: rgb(27, 27, 27);
+          text-align: center;
+          color: white;
+          padding: 2rem 0;
+        "
+      >
+        <h2 style="">Email Verification</h2>
+        <h3 style="width: 90%; margin: auto; font-size: small">
+          This is an email verification code from flip classroom, if you didn't
+          signup for this please ignore
+        </h3>
+        <p style="font-size: 50px">Code: ${val.message}</p>
+      </div>
+    </body>
+  </html>
+    `;
+};
