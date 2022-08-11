a8d53c1f3b7f08519c43cb449c480fd6b04a4915
Cedar-81 <64336732+Cedar-81@users.noreply.github.com>
2022-08-10 23:25:08 -1100

    bug fix

--- components/nav/sidenav.js
+++ components/nav/sidenav.js
@@ -24,10 +24,7 @@ function Sidenav() {
     }
   };
 
-  console.log(
-    teachername.split(" ")[0][0].toUpperCase() +
-      teachername.split(" ")[1][0].toUpperCase()
-  );
+  console.log(teachername);
 
   return (
     <div className="w-[12rem] bg-sidenav_bkg_color shadow-md relative z-[67] top-0 h-[100%]">
